c72660b0968c3d70f679f58227002106ab5189fc
glaseary <sofia.troncoso.m@gmail.com>
2025-06-14 15:03:48 -0400

    Primer commit, repositorio de apiUbicaciones.

---
+++ src/test/java/com/Perfulandia/ApiUbicaciones/ApiUbicacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiUbicaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUbicacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
